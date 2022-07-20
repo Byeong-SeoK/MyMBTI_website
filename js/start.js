65778c97500b80552832368c3eeee2e01875b0f2
LAPTOP-SEEN5R81\qudtj <qudtjr9849@gmail.com>
2022-07-21 00:23:51 +0900

    Add data file and construct QnA page

--- js/start.js
+++ js/start.js
@@ -1,6 +1,26 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 
+function addAnswer(answerText){
+    var a = document.querySelector('.answerBox');
+    var answer = document.createElement('button');//button이라는 것을 js를 통해서 만드는 것이다.
+    a.appendChild(answer);
+    //answer를 통해서 만든 버튼을 a라는 변수에 추가될 수 있도록 하는 것이다.
+    //즉, 만든 버튼들을 answerBox라는 id값을 가진 태그를 통해 보여주기 위해
+    //answerBox와 연결된 a라는 변수에 answer를 추가하는 것이다.
+    answer.innerHTML = answerText;
+}
+
+function goNext(qIdx){
+    var q = document.querySelector('.qBox');
+    q.innerHTML = qnaList[qIdx].q;
+    for(let i in qnaList[qIdx].a){
+        addAnswer(qnaList[qIdx].a[i].answer);//사용자에게 선택지를 보여주게하는 함수
+        //안의 파라미터는 data.js의 qnalist에서 qIdx번째의 질문에 대한 답변 태그 a에 대해
+        //i라는 for문 변수를 통해서 하나씩 접근하여 그 배열안의 answer 키값에 접근하는 것이다.
+    }
+}
+
 function begin(){
     main.style.WebkitAnimation = "fadeOut 1s";
     main.style.animation = "fadeOut 1s";
@@ -15,5 +35,7 @@ function begin(){
             qna.style.display = "block";
             //qna를 display 시키기 위해서 block 상태가 main이 none이 되고 난 이후 여야한다.
         },450);
+        let qIdx = 0;
+        goNext(qIdx);//사용자에게 질문지를 보여주는 함수
     }, 450);
 }
