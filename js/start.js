789f5c882e90d1169dc4ef8898600baf59851c9f
LAPTOP-SEEN5R81\qudtj <qudtjr9849@gmail.com>
2022-07-20 23:40:29 +0900

    Add QnA page

---
+++ js/start.js
@@ -0,0 +1,7 @@
+const main = document.querySelector("#main");
+const qna = document.querySelector("#qna");
+
+function begin(){
+    main.style.display = "none";
+    qna.style.display = "block";
+}
